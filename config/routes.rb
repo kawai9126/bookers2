40fbf5cdbd3edba30639b03fa6a47fa53e0e4b5e
kawai9126 <iroha.9126@gmail.com>
2021-05-15 11:19:54 +0000

    レイアウト前

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
+
   devise_for :users
+  root to: 'homes#top'
+  resources :books
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
