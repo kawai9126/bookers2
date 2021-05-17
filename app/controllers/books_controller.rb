77edd4dbed42a3b2cbdcb0b3db985659736f2574
kawai9126 <iroha.9126@gmail.com>
2021-05-17 12:53:26 +0000

    一覧

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,12 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @user = current_user
-    
-
+    @books = Book.all
   end
   def show
   end
   def edit
+  end
+  def create
   end
 end
