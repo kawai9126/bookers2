86921d0abeccfb56c6684b3ef3473cacaab3335a
kawai9126 <iroha.9126@gmail.com>
2021-05-18 08:56:36 +0000

    アソシエーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,23 @@ class BooksController < ApplicationController
     @user = current_user
     @books = Book.all
   end
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
   def show
+    @book = Book.find(params[:id])
+
   end
   def edit
   end
-  def create
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
