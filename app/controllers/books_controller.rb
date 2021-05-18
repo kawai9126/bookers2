41f01ab5a5b35b2c4d66c6fa051f19b0986c4a0c
kawai9126 <iroha.9126@gmail.com>
2021-05-18 11:14:40 +0000

    edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,15 @@ class BooksController < ApplicationController
   end
   def create
     book = Book.new(book_params)
+    book.user_id = current_user.id
     book.save
     redirect_to book_path(book.id)
   end
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = current_user
 
   end
   def edit
