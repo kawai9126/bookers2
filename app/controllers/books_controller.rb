599e5ff2fc8f58c580fd05ffe9d542f77cbba68b
kawai9126 <iroha.9126@gmail.com>
2021-05-18 12:18:18 +0000

    destroy action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,6 +19,11 @@ class BooksController < ApplicationController
   end
   def edit
   end
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
 
   private
