8c9d52846df8dca1e7170ea8f28553e68902b9b4
kawai9126 <iroha.9126@gmail.com>
2021-05-19 10:39:24 +0000

    見た目完了

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c0eb033bbb37c8570954758fd64b11e479a61ff52e7836eb605cba0c39e7f74ffc9d057d591b72ae470682e655e8921a4d8bf595818f8dc26ea5f0e2df1aae6c'
