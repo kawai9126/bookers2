441909ffb4677dc83c92229e3a3eab9bdecfde51
kawai9126 <iroha.9126@gmail.com>
2021-05-19 08:45:41 +0000

    usersok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
   def index
-
+    @book = Book.new
+    @user = current_user
+    @users = User.all
+  end
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to book_path(book.id)
   end
   def show
     @user = User.find(params[:id])
+    @books = @user.books.all
+
   end
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+
 end
