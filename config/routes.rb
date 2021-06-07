7b034e4d62f1576032d1964fbf0f7a072d9d8b1a
kawai9126 <iroha.9126@gmail.com>
2021-06-07 10:18:41 +0000

    あと一覧だけ

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
