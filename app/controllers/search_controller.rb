6cad3e6cd2cabeefc57e1316b46acc58f3bda105
kawai9126 <iroha.9126@gmail.com>
2021-06-08 10:43:45 +0000

    検索フォームはできて検索ができない

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,11 @@
 class SearchController < ApplicationController
+  def self.search(search) #self.でクラスメソッドとしている
+    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+      Book.user.where(['name LIKE ?', "%#{search}%"])
+    else
+      Book.all #全て表示。
+    end
+  end
 end
+
+
